65913094696848016b283a12b314b0830dfbc2c1
Dwyor-tmx <86127970+Dwyor-tmx@users.noreply.github.com>
2024-03-26 12:24:03 +0700

    Update sysctl.c
    ../kernel/sysctl.c:157:21: warning: unused variable 'ns_per_sec' [-Wunused-variable]
    static unsigned int ns_per_sec = NSEC_PER_SEC;
                        ^
    ../kernel/sysctl.c:158:35: warning: unused variable 'sysctl_sched_group_upmigrate_pct' [-Wunused-variable]
    static unsigned int __read_mostly sysctl_sched_group_upmigrate_pct = 100;
                                      ^
    ../kernel/sysctl.c:159:35: warning: unused variable 'sysctl_sched_group_downmigrate_pct' [-Wunused-variable]
    static unsigned int __read_mostly sysctl_sched_group_downmigrate_pct = 95;
                                      ^

--- kernel/sysctl.c
+++ kernel/sysctl.c
@@ -153,11 +153,11 @@ static int max_kswapd_threads = MAX_KSWAPD_THREADS;
 static int __maybe_unused two_hundred_million = 200000000;
 static int two_hundred_fifty_five = 255;
 
-#ifdef CONFIG_PELT_COMPATIBILITY_LAYER
-static unsigned int ns_per_sec = NSEC_PER_SEC;
-static unsigned int __read_mostly sysctl_sched_group_upmigrate_pct = 100;
-static unsigned int __read_mostly sysctl_sched_group_downmigrate_pct = 95;
-#endif /* CONFIG_PELT_COMPATIBILITY_LAYER */
+//#ifdef CONFIG_PELT_COMPATIBILITY_LAYER
+//static unsigned int ns_per_sec = NSEC_PER_SEC;
+//static unsigned int __read_mostly sysctl_sched_group_upmigrate_pct = 100;
+//static unsigned int __read_mostly sysctl_sched_group_downmigrate_pct = 95;
+//#endif /* CONFIG_PELT_COMPATIBILITY_LAYER */
 
 #ifdef CONFIG_SCHED_WALT
 const int sched_user_hint_max = 1000;
